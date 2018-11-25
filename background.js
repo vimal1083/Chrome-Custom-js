f3a60e882a63a68d9cbd584fbd37b2523b8f33c2
vimalraj <vimal1083@gmail.com>
2018-11-25 16:56:17 +0530

    Add changes to support ubuntu

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
 setInterval(() => {
     if ($("ytd-app")) {
         // Close banner in youtube video
-        $('.close-padding').click();
+        $('.close-padding') && $('.close-padding').click();
+        $('.ytp-ad-overlay-close-button') && $('.ytp-ad-overlay-close-button').click();
     
+
         // Remove youtube hero ad banner
-        $("iframe").remove();
+        $("iframe") && $("iframe").remove();
     
         // Skip advertisement
-        $(".videoAdUiSkipButton").click();
+        $(".videoAdUiSkipButton") && $(".videoAdUiSkipButton").click();
+        $(".ytp-ad-skip-button") && $(".ytp-ad-skip-button").click();
+
     
         // Remove legacy add near youtube videos
-        $(".ytd-companion-slot-renderer").remove();
+        $(".ytd-companion-slot-renderer") && $(".ytd-companion-slot-renderer").remove();
     }
 
     // Remove the google ads from all websites
-    $("div[id^=google_ads_iframe]").remove()
+    $("div[id^=google_ads_iframe]") && $("div[id^=google_ads_iframe]").remove()
     
 }, 500);
