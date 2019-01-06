e247e6bd72149fce4b5565604f986ba2a7f8fa9a
Vimal raj <vimal1083@gmail.com>
2019-01-06 18:12:53 +0530

    Fix typos

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 setInterval(() => {
     if ($("ytd-app") && $("ytd-app").length) {
+        
         // Close banner in youtube video
         $('.close-padding') && $('.close-padding').click();
         $('.ytp-ad-overlay-close-button') && $('.ytp-ad-overlay-close-button').click();
     
 
-        // Remove youtube hero ad banner
+        // Remove youtube hero Ad banner
         $("ytd-video-masthead-ad-renderer") && $("ytd-video-masthead-ad-renderer").remove();
     
         // Skip advertisement
@@ -13,14 +14,14 @@ setInterval(() => {
         $(".ytp-ad-skip-button") && $(".ytp-ad-skip-button").click();
 
     
-        // Remove legacy add near youtube videos
+        // Remove legacy Ad near youtube videos
         $(".ytd-companion-slot-renderer") && $(".ytd-companion-slot-renderer").remove();
     }
 
-    // Remove the google ads from all websites
+    // Remove the google Ads from all websites, mostly won't work
     $("div[id^=google_ads_iframe]") && $("div[id^=google_ads_iframe]").remove()
 
-    // Remove gmail ad banners
+    // Remove gmail Ad banners
     $(".aKB") && $(".aKB").remove()
     
 }, 500);
